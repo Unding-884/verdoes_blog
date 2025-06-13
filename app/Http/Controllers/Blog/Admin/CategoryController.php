376ce35203762485339a51ac51df8384d6491805
Unding <samanta.verdoes@gmail.com>
2025-06-13 13:04:05 +0300

    Лабораторна 6. Створення репозиторія та оптимізація запитів

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -5,20 +5,32 @@ namespace App\Http\Controllers\Blog\Admin;
 //use App\Http\Controllers\Controller;
 //use Illuminate\Http\Request;
 use App\Models\BlogCategory;
+use App\Repositories\BlogCategoryRepository;
 use Illuminate\Support\Str;
 use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Http\Requests\BlogCategoryCreateRequest;
 
 class CategoryController extends BaseController
 {
+        /**
+     * @var BlogCategoryRepository
+     */
+    private $blogCategoryRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         //dd(__METHOD__);
-        $paginator = BlogCategory::paginate(5);
+        //$paginator = BlogCategory::paginate(5);
 
+        $paginator = $this->blogCategoryRepository->getAllWithPaginate(5);
         return view('blog.admin.categories.index', compact('paginator'));
     }
 
@@ -27,9 +39,8 @@ class CategoryController extends BaseController
      */
     public function create()
     {
-        //dd(__METHOD__);
         $item = new BlogCategory();
-        $categoryList = BlogCategory::all();
+        $categoryList = $this->blogCategoryRepository->getForComboBox();
 
         return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
@@ -58,37 +69,32 @@ class CategoryController extends BaseController
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //dd(__METHOD__);
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+
+       public function edit($id)
     {
-        //dd(__METHOD__);
-        $item = BlogCategory::findOrFail($id);
-        $categoryList = BlogCategory::all();
+        $item = $this->blogCategoryRepository->getEdit($id);
+        if (empty($item)) {
+            abort(404);
+        }
+        $categoryList = $this->blogCategoryRepository->getForComboBox($item->parent_id);
 
         return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(BlogCategoryUpdateRequest $request, $id)
     {
-        //dd(__METHOD__);
-        $item = BlogCategory::find($id);
-        if (empty($item)) { //якщо ід не знайдено
-            return back() //redirect back
-                ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"]) //видати помилку
-                ->withInput(); //повернути дані
+
+        $this->blogCategoryRepository->getEdit($id);
+        if (empty($item)) {
+            return back()
+                ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"])
+                ->withInput();
         }
 
         $data = $request->all(); //отримаємо масив даних, які надійшли з форми
